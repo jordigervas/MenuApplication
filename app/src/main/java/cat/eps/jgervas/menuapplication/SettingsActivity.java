5f88af121dcc79d3f087f95ca696059aafff16d6
jordigervas <jordi.gervas@gmail.com>
2016-11-08 19:40:32 +0100

    Initial commit

---
+++ app/src/main/java/cat/eps/jgervas/menuapplication/SettingsActivity.java
@@ -0,0 +1,17 @@
+package cat.eps.jgervas.menuapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SettingsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        getFragmentManager().beginTransaction()
+                .replace(R.id.container, new SettingsFragment())
+                .commit();
+    }
+}
